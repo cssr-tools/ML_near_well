BUG: Run H2O example with more ensemble members to fix wrong results in the first example.

runscripts/ECMOR24_proceeding/co2_2d/main.py
@@ -58,7 +58,7 @@ FLOW_ML: pathlib.Path = (
 
 
 # Run ensemble and extract data.
-if False:
+if True:
     extracted_data: np.ndarray = full_ensemble(
         runspecs_ensemble,
         ensemble_dir,

runscripts/ECMOR24_proceeding/co2_3d/main.py
@@ -58,7 +58,7 @@ for integration_dir in [
     integration_dir.mkdir(parents=True, exist_ok=True)
 
 # Run ensemble and extract data.
-if False:
+if True:
     extracted_data: np.ndarray = full_ensemble(
         runspecs_ensemble,
         ensemble_dir,
@@ -78,7 +78,7 @@ if False:
     np.save(str(ensemble_dir / "features"), extracted_data)
 
 # Upscale and create dataset.
-if False:
+if True:
     extracted_data: np.ndarray = np.load(str(ensemble_dir / "features.npy"))
     upscaler: CO2_3D_upscaler = CO2_3D_upscaler(extracted_data, runspecs_ensemble, 6)
     features, targets = upscaler.create_ds(ensemble_dir, step_size_x=3, step_size_t=3)
@@ -86,7 +86,7 @@ if False:
     restructure_data(data_dir, data_stencil_dir, trainspecs, stencil_size=3)
 
 # Plot some WIs.
-if False:
+if True:
     features, targets = reload_data(
         runspecs_ensemble,
         trainspecs,
@@ -125,7 +125,7 @@ if False:
         )
 
 # Tune and train model.
-if False:
+if True:
     tune_and_train(
         trainspecs,
         data_stencil_dir,
@@ -138,14 +138,14 @@ if False:
     )
 
 # Do some plotting of results and sensitivity analysis.
-if False:
+if True:
     model: keras.Model = keras.models.load_model(nn_dir / "bestmodel.keras")  # type: ignore
     features, targets = reload_data(
         runspecs_ensemble,
         trainspecs,
         data_stencil_dir,
         # A lot of the outer cells got disregarded during upscaling, because the
-        # saturation could not be fully upscaled. -> Only 5 x values.
+        # saturation could not be fully upscaled. -> Only 11 x values.
         num_xvalues=11,
         step_size_t=3,
     )
@@ -192,7 +192,7 @@ if False:
     )
 
 # Integrate into OPM.
-if False:
+if True:
     integration.recompile_flow(
         nn_dir / "scalings.csv",
         runspecs_integration_3D_and_Peaceman_1["constants"]["OPM"],

runscripts/ECMOR24_proceeding/co2_3d/main_comparison_w_2d.py
@@ -89,34 +89,8 @@ if True:
 # Upscale and create dataset.
 if True:
     extracted_data: np.ndarray = np.load(str(ensemble_dir / "features.npy"))
-    # Manually disregard runs that produce zero pressure differences between the
-    # well cell and other grid cells and thus infinite WR. For some reason these were
-    # not stopped during running.
-    extracted_data_per_zcell: np.ndarray = extracted_data.reshape(
-        extracted_data.shape[0],
-        extracted_data.shape[1],
-        runspecs_ensemble["constants"]["NUM_ZCELLS"],
-        -1,
-        extracted_data.shape[3],
-    )
-    invalid_members: list[int] = list(
-        set(
-            np.argwhere(
-                np.expand_dims(extracted_data_per_zcell[..., 0, 0], axis=-1)
-                - extracted_data_per_zcell[..., 1:, 0]
-                == 0
-            )[..., 0]
-        )
-    )
-    extracted_data = np.delete(extracted_data, invalid_members, axis=0)
     upscaler: CO2_3D_upscaler = CO2_3D_upscaler(extracted_data, runspecs_ensemble, 6)
     features, targets = upscaler.create_ds(ensemble_dir, step_size_x=3, step_size_t=3)
-
-    # Some WI will be negative for some reason. Remove these.
-    invalid_members = list(set(np.argwhere(targets <= 0)[..., 0]))
-    features = np.delete(features, invalid_members, axis=0)
-    targets = np.delete(targets, invalid_members, axis=0)
-
     ensemble.store_dataset(features, targets, data_dir)
     restructure_data(data_dir, data_stencil_dir, trainspecs, stencil_size=3)
 
@@ -127,8 +101,8 @@ if True:
         trainspecs,
         data_stencil_dir,
         # A lot of the outer cells got disregarded during upscaling, because the
-        # saturation could not be fully upscaled. -> Only 5 x values.
-        num_xvalues=8,
+        # saturation could not be fully upscaled. -> Only 11 x values.
+        num_xvalues=11,
         step_size_t=3,
     )
     for i in range(0, features.shape[0], 20):
@@ -159,18 +133,20 @@ if True:
             y_param="WI_log",
         )
 
-# Train model and do some plotting of results and analysis.
+# Tune and train model.
 if True:
     tune_and_train(
         trainspecs,
         data_stencil_dir,
         nn_dir,
         max_trials=5,
-        lr=1e-4,
+        lr=1e-3,
+        lr_tune=1e-4,
         epochs=1000,
         executions_per_trial=1,
     )
 
+# Do some plotting of results and sensitivity analysis.
 if True:
     model: keras.Model = keras.models.load_model(nn_dir / "bestmodel.keras")  # type: ignore
     features, targets = reload_data(
@@ -178,8 +154,8 @@ if True:
         trainspecs,
         data_stencil_dir,
         # A lot of the outer cells got disregarded during upscaling, because the
-        # saturation could not be fully upscaled. -> Only 5 x values.
-        num_xvalues=8,
+        # saturation could not be fully upscaled. -> Only 11 x values.
+        num_xvalues=11,
         step_size_t=3,
     )
     for i in range(0, features.shape[0], 20):

runscripts/ECMOR24_proceeding/h2o/main.py
@@ -48,7 +48,7 @@ for integration_dir in [integration_dir_1, integration_dir_2, integration_dir_3]
     integration_dir.mkdir(parents=True, exist_ok=True)
 
 # Run ensemble and extract data.
-if False:
+if True:
     data: np.ndarray = full_ensemble(
         runspecs_ensemble,
         ensemble_dir,
@@ -72,7 +72,7 @@ if False:
 
 
 # Create dataset.
-if False:
+if True:
     # Truncate the outermost cell already here to avoid getting nan in the WIs.
     data = np.load(str(ensemble_dir / "data.npy"))[..., :-1, :]
     # Get radii and transform from triangle grid to cake grid.
@@ -130,20 +130,20 @@ if False:
         )
 
 # Tune and train model.
-if False:
+if True:
     tune_and_train(
         trainspecs,
         data_dir,
         nn_dir,
         max_trials=20,
-        lr=1e-4,
+        lr=1e-3,
         lr_tune=1e-4,
         epochs=5000,
         executions_per_trial=1,
     )
 
 # Do some plotting of results and sensitivity analysis.
-if False:
+if True:
     model: keras.Model = keras.models.load_model(nn_dir / "bestmodel.keras")  # type: ignore
     for i in range(0, runspecs_ensemble["npoints"], 30):
         # Plot NN WI and data WI vs radius.
@@ -196,9 +196,7 @@ if True:
 
 # Plot results.
 if True:
-    for integration_dir in [
-        integration_dir_1
-    ]:  # , integration_dir_2, integration_dir_3]:
+    for integration_dir in [integration_dir_1, integration_dir_2, integration_dir_3]:
         summary_files: list[pathlib.Path] = [
             integration_dir / "run_6" / "output" / "5X5M_PEACEMAN.SMSPEC",
             integration_dir / "run_0" / "output" / "100X100M_NN.SMSPEC",